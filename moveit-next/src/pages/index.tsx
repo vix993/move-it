b3f93e7c62665f4bf3434003a58ce171a5eb3e41
Victor <victor.as.nascimento@gmail.com>
2021-02-24 08:36:43 -0300

    next project

---
+++ moveit-next/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import {ExperienceBar} from '../components/ExperienceBar';
+
+
+export default function Home() {
+  return (
+    <div className="h-screen max-w-screen-lg my-0 mx-auto py-10 px-8 flex flex-col">
+      <ExperienceBar/>
+    </div>
+  )
+}
--- moveit/src/components/ExperienceBar.tsx
+++ moveit/src/components/ExperienceBar.tsx
@@ -1,14 +1,14 @@
 export const ExperienceBar = () => {
-    return (
-        <header className="flex items-center">
-            <span className="text-base text-gray-500">0 xp</span>
-            <div className="flex-1 h-1 rounded-t bg-gray-300 mx-6 relative">
-                <div className="w-4/12 h-1 bg-green-400" />
-                <span className="text-gray-500 left-1/2 absolute top-px transform -translate-x-1/2">
-                    300px
-                </span>
-            </div>
-            <span className="text-base text-gray-500">600 xp</span>
-        </header>
-    )
-}
+  return (
+    <header className="flex items-center">
+      <span className="text-base text-gray-500">0 xp</span>
+      <div className="flex-1 h-1 rounded-t bg-gray-300 mx-6 relative">
+        <div className="w-4/12 h-1 bg-green-400" />
+        <span className="text-gray-600 left-1/2 absolute top-px transform -translate-x-1/2">
+          300px
+        </span>
+      </div>
+      <span className="text-base text-gray-500">600 xp</span>
+    </header>
+  );
+};
